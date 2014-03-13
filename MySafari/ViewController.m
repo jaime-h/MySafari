e4ec4bc662068153ecfddd781ca8e09527b088ad
jaime-h <jaime.e.hernandez@gmail.com>
2014-03-12 19:42:55 -0500

    Almost finished

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -32,11 +32,13 @@
     
     self.myWebView.scrollView.delegate = self;
     
-    
-	// Do any additional setup after loading the view, typically from a nib.
+
 }
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
+    // concatenate HTTP to the address and pass it.
+    // need error checking here if they pass the http:// etc .....
+    
     NSString *appendHTTPer =@"HTTP://";
     appendHTTPer = [appendHTTPer stringByAppendingString:textField.text];
     NSString *completAddress = appendHTTPer;
@@ -46,37 +48,41 @@
     
     [self.myWebView loadRequest:request];
     
+    [self.myURLTextField resignFirstResponder];
+    
     return YES;
 }
 
 - (IBAction)onBackButtonPressed:(id)sender
 {
-    
     [self.myWebView goBack];
-    
 }
 
 - (IBAction)onForwardButton:(id)sender
 {
-   
     [self.myWebView goForward];
 }
 
 - (IBAction)onStopButtonPressed:(id)sender
 {
-    
     [self.myWebView stopLoading];
 }
 
 - (IBAction)onReloadButtonPressed:(id)sender
 {
-    
     [self.myWebView reload];
 }
 
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+    // set spinner to on
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+}
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
+    // set spinner to off
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
     
     if ([self.myWebView canGoForward]) {
         self.forwardButton.enabled = YES;
@@ -87,9 +93,9 @@
     }
     
     // http://stackoverflow.com/questions/2491410/get-current-url-of-uiwebview
-    // Getting the Title page using a Javascript and then parsing it then setting it to the url field
-    self.myURLTextField.text = [self.myWebView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    // Getting the Title page using Javascript and then parsing it then setting it to the url field
     
+    self.myURLTextField.text = [self.myWebView stringByEvaluatingJavaScriptFromString:@"document.title"];
 }
 
 - (IBAction)plusButtonPressed:(id)sender {
@@ -103,14 +109,22 @@
     
     // The scrollViewDidScrollToTop was not getting called so..
     // we are testing the view offset and calling it when it's at the top.
-    if (scrollView.contentOffset.y == 0) {
-        
-        self.myURLTextField.alpha = 0.99;
+    
+    // While scrolling make the address bar opaque - reanimate when stopped.
+    
+    self.myURLTextField.alpha = 0.0;
+    if (self.myWebView.scrollView.decelerating == YES) {
+        [self.myURLTextField resignFirstResponder];
+        self.myURLTextField.alpha = 1.0;
     }
-    else
-    {
-        self.myURLTextField.alpha = 0.0;
-    }
+//    
+//    if (scrollView.contentOffset.y == 0) {
+//        self.myURLTextField.alpha = 0.99;
+//    }
+//    else
+//    {
+//        self.myURLTextField.alpha = 0.0;
+//    }
 }
 
 @end
